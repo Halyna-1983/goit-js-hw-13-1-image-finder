82204e65e83eab6e1ce89bcb1a4ff322e99bba95
Halyna-1983 <72556196+Halyna-1983@users.noreply.github.com>
2021-04-27 12:44:37 +0300

    scrollTo added

--- src/index.js
+++ src/index.js
@@ -3,16 +3,17 @@ import ApiService from './apiService';
 import templates from './templates/imageCard.hbs';
 import './css/styles.css'
 
+
 const refs = {
     searchForm: document.querySelector('.search-form'),
     gallery: document.querySelector('.gallery'),
     loadMoreBtn: document.querySelector('[data-action="load-more"]')
 };  
 const apiService = new ApiService();
-//console.log(apiService);
     
 refs.searchForm.addEventListener('submit', onSearch);
 refs.loadMoreBtn.addEventListener('click', onLoadMore);
+refs.loadMoreBtn.addEventListener('click', scrollToHandler);
 
 function onSearch(event){
         event.preventDefault();
@@ -25,14 +26,21 @@ function onSearch(event){
 function onLoadMore(){
     apiService.fetchImage().then(appendGalleryHits);
 }
+
 function appendGalleryHits(hits) {
     refs.gallery.insertAdjacentHTML('beforeend', templates(hits));
-  }
+}
+
 function cleargallery() {
     refs.gallery.innerHTML = '';
-  }
+}
 
-  window.scrollTo({
-    top: 100,
-    behavior: "smooth"
-});
+function scrollToHandler(){
+    //console.log(window.pageYOffset);
+  return window.scrollTo({
+      top: 550,
+      behavior: "smooth",
+  });
+}
+
+

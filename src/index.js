b72acc5cc16edbdf588a097c1cae75e053551b2d
Halyna-1983 <72556196+Halyna-1983@users.noreply.github.com>
2021-04-27 14:49:46 +0300

    scroll lookes good now

--- src/index.js
+++ src/index.js
@@ -13,7 +13,9 @@ const apiService = new ApiService();
     
 refs.searchForm.addEventListener('submit', onSearch);
 refs.loadMoreBtn.addEventListener('click', onLoadMore);
-refs.loadMoreBtn.addEventListener('click', scrollToHandler);
+//refs.loadMoreBtn.addEventListener('click', scrollToHandler);
+
+let scrollValue = 0;
 
 function onSearch(event){
         event.preventDefault();
@@ -28,6 +30,7 @@ function onLoadMore(){
 }
 
 function appendGalleryHits(hits) {
+    scrollPages();
     refs.gallery.insertAdjacentHTML('beforeend', templates(hits));
 }
 
@@ -35,12 +38,27 @@ function cleargallery() {
     refs.gallery.innerHTML = '';
 }
 
-function scrollToHandler(){
-    //console.log(window.pageYOffset);
-  return window.scrollTo({
-      top: 550,
-      behavior: "smooth",
-  });
-}
+// function scrollToHandler(){
+//     //console.log(window.pageYOffset);
+//   return window.scrollTo({
+//       top: 550,
+//       behavior: "smooth",
+//   });
+// }
 
+// function appendGalleryHits(hits) {
+// refs.gallery.insertAdjacentHTML('beforeend', templates(hits));
+// }
+
+function scrollPages() {
+//получаем высоту страницы
+const scrollValue = document.documentElement.scrollHeight;
+//реализуем асинхронный scrollTo, чтобы метод корректно работал
+setTimeout(() => {
+window.scrollTo({
+top: scrollValue,
+behavior: 'smooth',
+});
+}, 100);
+}
 
